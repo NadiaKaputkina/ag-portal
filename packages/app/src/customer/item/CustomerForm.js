96aff48616ab450af431a197bda2be6c91a334cf
Nadezhda Kaputkina <kaputkinanv@oxagile.com>
2021-08-02 19:13:27 +0300

    add editable text field

--- packages/app/src/customer/item/CustomerForm.js
+++ packages/app/src/customer/item/CustomerForm.js
@@ -1,13 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import { Button } from "@material-ui/core";
+import { useFormik } from 'formik';
+import EditableTextInput from "./EditableTextInput";
 
 const CustomerForm = (props) => {
-    const {
-        customer
-    } = props;
-    
+    const { customer } = props;
+
+    const formik = useFormik({
+        initialValues: {
+            name: customer ? customer.name : '',
+            age: customer ? customer.age : '',
+        },
+        enableReinitialize: true,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    const [currentFieldName, setCurrentFieldName] = useState(null)
+
+    if (!customer) return null;
+
     return (
-        <div>{ JSON.stringify(customer) }</div>
-    )
-}
+        <div style={{ width: "100%" }}>
+            <form onSubmit={formik.handleSubmit}>
+                <Grid container spacing={1}>
+                    <Grid container item xs={12} spacing={3}>
+                        <Grid item xs={3}>
+                            Id
+                        </Grid>
+                        <Grid item xs={6}>
+                            {customer.id}
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <Grid item xs={3}>
+                            Имя
+                        </Grid>
+                        <Grid item xs={6}>
+                            <EditableTextInput 
+                                currentFieldName={currentFieldName}
+                                setCurrentFieldName={setCurrentFieldName}
+                                fieldName={'name'}
+                                formik={formik}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <Grid item xs={3}>
+                            Возраст
+                        </Grid>
+                        <Grid item xs={6}>
+                            {customer.age}
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <Button color="primary" variant="contained" fullWidth type="submit">
+                            Submit
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </div>
+    );
+};
 
 export default CustomerForm;
